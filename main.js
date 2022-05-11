001ab337221e71ac2bb04403cdb9a2b735b36774
Naisha165 <98308753+Naisha165@users.noreply.github.com>
2022-05-11 15:05:17 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+canvas = document.getElementById("myCanvas");
+color ="yellow";
+
+ctx. canvas.getContext("2d");
+ctx.beginPath();
+ctx.strokeStyle = color;
+ctx.lineWidth =2;
+ctx.arc (200, 200, 40, 2 *Math.PI);
+ctx.stroke();
+
+mouse_x = e.clientX;
+mouse_y = e.clientY;
+
+canvas.addEventListener ("mousedown", my_mousedown);
+
+function my_mousedown (e)
+{
+  mouse_x= e.clientX -canvas.offsetLeft;
+  
+  mouse_y= e.clientY -canvas.offsetTop;
+  console.log("X=" +mouse_x + "Y="+mouse_y);
+  circle (mouse_x, mouse_y);
+
+}
+
+function circle(mouse_x, mouse_y)
+{
+    ctx.beginPath();
+    ctx.strokeStyle = color;
+    ctx.lineWidth =2;
+    ctx.arc (mouse_x, mouse_y, 40, 2 *Math.PI);
+    ctx.stroke(); 
+}
